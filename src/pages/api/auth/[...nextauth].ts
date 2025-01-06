9639f3f5e1812b1b62575761e50b9e1805696159
hnascx <henriquehusky@gmail.com>
2025-01-06 08:42:03 -0300

    feat/Github Auth concluded

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,35 @@
 import { PrismaAdapter } from "@/lib/auth/prismaAdapter";
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider, { GithubProfile } from 'next-auth/providers/github'
 
 export function buildNextAuthOptions(req: NextApiRequest, res: NextApiResponse): NextAuthOptions {
   return {  
     adapter: PrismaAdapter(req, res),
 
-    providers: []
+    providers: [
+      GithubProvider({  
+        clientId: process.env.GITHUB_CLIENT_ID ?? "",
+        clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+        profile: (profile: GithubProfile) => {
+          return {
+            id: profile.id,
+            name: profile.name!,
+            email: profile.email!,
+            avatar_url: profile.avatar_url
+          }
+        }
+      })
+    ],
+
+    callbacks: {
+      async session ({ session, user }) {
+        return {
+          ...session,
+          user
+        }
+      }
+    }
   }
 }
 
