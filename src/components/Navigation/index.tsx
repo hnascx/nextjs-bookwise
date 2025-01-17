35e21211521160b26c3ae66228ec13b51ad0f34d
hnascx <henrique.nascimento12@hotmail.com>
2025-01-17 17:02:24 -0300

    feat/Navigation on sidebar concluded

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,26 +1,42 @@
 import { Binoculars, ChartLineUp, User } from "@phosphor-icons/react"
 import { Container, NavItemContainer } from "./styles"
 import { useRouter } from "next/router"
+import { useMemo } from "react"
+import { useSession } from "next-auth/react"
 
 const NAV_ITEMS = [
   {
     label: "Home",
     href: "/",
-    icon: <ChartLineUp />
+    icon: <ChartLineUp width={24} />
   },
   {
     label: "Explorar",
     href: "/explore",
-    icon: <Binoculars />
+    icon: <Binoculars width={24} />
   },
 ]
 
 export const Navigation = () => {
   const router = useRouter()
 
+  const { data: session } = useSession()
+
+  const navItems = useMemo(() => {
+    if (session) {
+      return NAV_ITEMS.concat({
+        label: "Perfil",
+        href: `/profile/${session.user.id}`,
+        icon: <User size={24} />
+      })
+    }
+
+    return NAV_ITEMS
+  }, [session])
+
   return (
     <Container>
-      {NAV_ITEMS.map(({ href, label, icon }) => (
+      {navItems.map(({ href, label, icon }) => (
         <NavItemContainer href={href} key={label} active={router.asPath === href}>
           {icon}
           {label}
