4786358d59588535a9f5333237b844272b919f0e
hnascx <henrique.nascimento12@hotmail.com>
2025-01-23 16:32:28 -0300

    feat/Working on explore page

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,11 +1,54 @@
-import { ExploreContainer } from "@/styles/pages/explore";
+import { BooksGrid, ExploreContainer, TagsContainer } from "@/styles/pages/explore";
 import { NextPageWithLayout } from "./_app";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
+import { PageTitle } from "@/components/ui/PageTitle";
+import { Binoculars } from "@phosphor-icons/react";
+import { Input } from "@/components/ui/Form/Input";
+import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
+import { useState } from "react";
+import { Tag } from "@/components/ui/Tag";
+import { BookCard } from "@/components/BookCard";
+import { useQuery } from "@tanstack/react-query";
+import { Category } from "@prisma/client";
+import { api } from "@/lib/axios";
 
 const ExplorePage: NextPageWithLayout = () => {
+  const [search, setSearch] = useState("")
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+
+  const { data: categories } = useQuery<Category[]>(["categories"], async () => {
+    const { data } = await api.get("/books/categories")
+    return data.categories ?? []
+  })
+
   return (
     <ExploreContainer>
+      <header>
+        <PageTitle title="Explorar" icon={<Binoculars size={32} />} />
 
+        <Input
+          placeholder="Buscar livro ou autor"
+          icon={<MagnifyingGlass size={20} />}
+          css={{ maxWidth: 433 }}
+          value={search}
+          onChange={({ target }) => setSearch(target.value)}
+        />
+      </header>
+
+      <TagsContainer>
+        <Tag active={selectedCategory === null} onClick={() => setSelectedCategory(null)}>
+          Tudo
+        </Tag>
+        {categories?.map(category => (
+          <Tag key={category?.id} active={selectedCategory === category.id} onClick={() => setSelectedCategory(category.id)}>
+            {category?.name}
+          </Tag>
+        ))}
+      </TagsContainer>
+
+      <BooksGrid>
+        {/* <BookCard size="lg" book={}/> */}
+      </BooksGrid>
     </ExploreContainer>
   )
 }
